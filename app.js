5d45166188d763ecad364fa365d9bba1f923ebc0
infosalim <salimh5757@gmail.com>
2019-07-20 20:37:25 +0600

    argument parsing with yargs

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-const fs = require('fs');
+const getNotes = require('./notes');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
-fs.appendFileSync('note.txt', `- Salim Hossain`);
+// Customize yargs version
+yargs.version('1.1.0');
+
+// add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log('Adding a new note!');
+    }
+});
+
+// remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: () => {
+        console.log('Removing a note!');
+    }
+});
+
+// list command
+yargs.command({
+    command: 'list',
+    describe: 'List of your note',
+    handler: () => {
+        console.log('Listing out all notes!');
+    }
+});
+
+// read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log('Reading a note!');
+    }
+})
+
+console.log(yargs.argv);
