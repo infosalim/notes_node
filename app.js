de81f08650bb28ad24f7469cda30de1fba7a26a1
infosalim <salimh5757@gmail.com>
2019-07-22 00:37:54 +0600

    added a note

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const getNotes = require('./notes');
 const chalk = require('chalk');
 const yargs = require('yargs');
+const notes = require('./notes');
 
 // Customize yargs version
 yargs.version('1.1.0');
@@ -22,8 +22,7 @@ yargs.command({
         }
     },
     handler: (argv) => {
-        console.log('Title:', argv.title);
-        console.log('Body:', argv.body);
+        notes.addNote(argv.title, argv.body);
     }
 });
 
